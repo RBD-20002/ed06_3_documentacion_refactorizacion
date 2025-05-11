e6cb66e759e5ad3234067532a6bb609b23b0d6c7
RICARDO <RICARDO03-03-02@HOTMAIL.com>
2025-05-11 18:23:20 +0200

    Refactorización en las clases

--- src/main/java/org/ed06/app/Main.java
+++ src/main/java/org/ed06/app/Main.java
@@ -4,10 +4,7 @@ import org.ed06.model.*;
 /** Main limpio **/
 public class Main {
     public static void main(String[] args) {
-        Hotel hotel = new Hotel(
-                "El Mirador",
-                "Calle Entornos de Desarrollo 6",
-                "123456789"
+        Hotel hotel = new Hotel("Sharenton", "Calle Palmeras 159", "88888888"
         );
         new HotelCLI(hotel).inicio();
     }
